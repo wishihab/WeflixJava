ca4e3a552afa2ff32f5fb41c4f16553c90f40b93
Alwi <wishihab@gmail.com>
2022-10-03 17:11:12 +0700

    Membuat model movie popular

---
+++ app/src/main/java/com/wishihab/weflixjava/model/general/MoviePopularResult.java
@@ -0,0 +1,129 @@
+package com.wishihab.weflixjava.model.general;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class MoviePopularResult implements Serializable {
+
+    @SerializedName("adult")
+    private boolean adult;
+    @SerializedName("backdrop_path")
+    private String backdropPath;
+    @SerializedName("id")
+    private String id;
+    @SerializedName("original_language")
+    private String originalLanguage;
+    @SerializedName("original_title")
+    private String originalTitle;
+    @SerializedName("overview")
+    private String overview;
+    @SerializedName("popularity")
+    private Double popularity;
+    @SerializedName("release_date")
+    private String releaseDate;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("video")
+    private boolean video;
+    @SerializedName("vote_average")
+    private Double voteAverage;
+    @SerializedName("vote_count")
+    private Integer voteCount;
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public void setVideo(boolean video) {
+        this.video = video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+}
